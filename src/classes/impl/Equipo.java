6e145f9300d8a00670eeb10e3a206aaf52e9faa1
Pablo <pmacsal2707@g.educaand.es>
2024-03-03 20:18:28 +0100

    Corrección clase Equipo by Pablo

--- src/classes/impl/Equipo.java
+++ src/classes/impl/Equipo.java
@@ -5,60 +5,60 @@ import java.util.Objects;
 
 public class Equipo implements Comparable<Equipo> {
 
+    // Atributos
     private String nombre;
     private ArrayList<Participante> jugadores;
     private int puntos;
 
     // Constructor
     public Equipo(String nombre){
+        this.nombre = nombre;
         this.jugadores = new ArrayList<>();
         this.puntos = 0;
     }
 
-    // Metodo para añadir un jugador al equipo
+    // Método para añadir un jugador al equipo
     public boolean anadirJugador(Participante jugador) {
-        boolean sePuedeAnadir = true;
-        for (int i=0; i<jugadores.size(); i++) {
-            if (jugadores.get(i).getDni().equals(jugador.getDni())) {
-                System.out.println("ERROR: El jugador ya se ha registrado anteriormente");
-                sePuedeAnadir = false;
-                break;
-            }
-        }
-        if (sePuedeAnadir) {
+        if (jugadores.contains(jugador)) { // Verificamos si el jugador ya está en el equipo
+            System.out.println("ERROR: El jugador ya se ha registrado anteriormente");
+            return false;
+        } else { // Si no está lo añadimos
             jugadores.add(jugador);
+            return true;
         }
-        return sePuedeAnadir;
     }
 
-    // Metodo para eliminar un jugador de la lista de jugadores
+    // Método para eliminar un jugador de la lista de jugadores
     public boolean eliminarJugador(Participante jugador) {
-        boolean sePuedeEliminar = true;
-        for (int i=0; i<jugadores.size(); i++) {
-            if (jugadores.get(i).getDni().equals(jugador.getDni())) {
-                sePuedeEliminar = true;
-                System.out.println("El jugador "+jugador.getNombre()+" se ha eliminado correctamente");
-                break;
-            }
+        if (!jugadores.contains(jugador)) { // Verificamos si el jugador está en la lista
+            System.out.println("ERROR: El jugador no está registrado en este equipo"); // En caso de que no esté devolvemos un mensaje de error
+            return false;
         }
-        return sePuedeEliminar;
+        // Si se encuentra el jugador lo eliminamos de la lista
+        jugadores.remove(jugador);
+        System.out.println("El jugador "+jugador.getNombre()+" se ha eliminado correctamente");
+        return true;
     }
 
-    // Metodo toString
+    // Método toString
     @Override
     public String toString(){
-        return  "nombre='" + this.nombre + "\n"+
-                ", puntos='" + this.puntos + "\n"+
-                ", jugadores='" + this.jugadores.size() + "\n";
+        String resultado = "Equipo: " + nombre + "\n";
+        resultado += "Puntos acumulados: " + puntos + "\n";
+        resultado += "Jugadores:\n";
+        for (int i = 0; i < jugadores.size(); i++) {
+            resultado += "- " + jugadores.get(i) + "\n";
+        }
+        return resultado;
     }
 
-    // Metodo equals
+    // Método equals
     @Override
     public boolean equals(Object team) {
         if (team instanceof Equipo) {
             Equipo team2 =  (Equipo) team;
 
-            if (this.nombre.equals(team2.nombre)) {
+            if (this.nombre.equalsIgnoreCase(team2.nombre)) {
                 return true;
             } else {
                 return false;
@@ -68,7 +68,7 @@ public class Equipo implements Comparable<Equipo> {
         }
     }
 
-    // Metodo compareTo
+    // Método compareTo
     @Override
     public int compareTo(Equipo eq) {
         if (this.puntos == eq.puntos) {
