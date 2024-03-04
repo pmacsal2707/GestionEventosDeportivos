84cb83eee10e2992d775cfb964c4feab9f3e9eb7
Pablo <pmacsal2707@g.educaand.es>
2024-03-04 22:44:13 +0100

    Comentarios ganador

--- src/classes/api/Ganador.java
+++ src/classes/api/Ganador.java
@@ -4,9 +4,16 @@ import classes.impl.Participante;
 
 import java.util.ArrayList;
 
+/**
+ * Interfaz que define un método para obtener el ganador de un evento deportivo
+ */
 public interface Ganador {
 
     // Métodos
+    /**
+     * Método para obtener el ganador del evento
+     * @return un arrayList de participantes que representan al ganador del evento
+     */
     ArrayList<Participante> obtenerGanador();
 
 }
